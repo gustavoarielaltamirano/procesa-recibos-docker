5966e2f761fe92ee15cd56f4b32fc92c0df73c58
Gustavo Altamirano <gustavoarielaltamirano@gmail.com>
2023-01-10 21:50:24 -0300

    Proceso de identificacion de archivo zip con recibos en docker

--- get_files_to_process.py
+++ get_files_to_process.py
@@ -1,7 +1,10 @@
 import os
 import fnmatch
 import shutil
+from smb.base import SharedFile
 from smb.SMBConnection import SMBConnection
+
+listSharedFileObj = []
 
 # Connect to the Windows share network directory
 server_name = "192.168.10.236"
@@ -20,19 +23,35 @@ NET_WIN_DIR = "GrupoDoc\\Documentos Compartidos\\BOTSRECIBOSGG"
 # Define share network dir for processed files
 NET_WIN_DIR_FINAL = 'GrupoDoc\\Documentos Compartidos\\BOTSRECIBOSGG\\listos'
 
+with SMBConnection(user_name, password, my_name=host, remote_name=remote_host, domain=domain) as smbconn:
+    # "You must call this method before attempting any of the file operations with the remote server."
+    # https://pysmb.readthedocs.io/en/latest/api/smb_SMBConnection.html#smb.SMBConnection.SMBConnection.connect
+    smbconn.connect(remote_host)
+
+    # Returns: A list of "smb.base.SharedFile" instances
+    # https://pysmb.readthedocs.io/en/latest/api/smb_SMBConnection.html#smb.SMBConnection.SMBConnection.listPath
+    listSharedFileObj = smbconn.listPath("GrupoDoc", "/Documentos Compartidos/BOTSRECIBOSGG")
+
+
+
 
 def get_files():
+    # Pre annotate for type-hinting SharedFile object
+    item: SharedFile
 
-    current_path = os.getcwd()
+    for item in listSharedFileObj:
+        print(item.filename)
+
+    # current_path = os.getcwd()
 
     # List the contents of the directory
-    zip_to_process = client.listPath(NET_WIN_DIR, "/")
-
-    for file in zip_to_process:
-        if fnmatch.fnmatch(file, '*.zip'):
-            # file_path = os.path.join(root, file)
-            shutil.move(NET_WIN_DIR+'\\'+file, current_path+'/resultados/')
-            print('Archivo zip identificado: ', file)
-
-    if zip_to_process == '':
-        print('No hay archivos zip para procesar, se vuelve a consultar en 1 min...')
+    # zip_to_process = client.listPath(NET_WIN_DIR, "/")
+    #
+    # for file in zip_to_process:
+    #     if fnmatch.fnmatch(file, '*.zip'):
+    #         # file_path = os.path.join(root, file)
+    #         shutil.move(NET_WIN_DIR+'\\'+file, current_path+'/resultados/')
+    #         print('Archivo zip identificado: ', file)
+    #
+    # if zip_to_process == '':
+    #     print('No hay archivos zip para procesar, se vuelve a consultar en 1 min...')
