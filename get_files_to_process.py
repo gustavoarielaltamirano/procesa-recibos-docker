d04dce3e523127ad724790e466b81fa8ba09d6b1
Gustavo Altamirano <gustavoarielaltamirano@gmail.com>
2023-01-10 22:07:18 -0300

    Proceso de identificacion de archivo zip con recibos en docker

--- get_files_to_process.py
+++ get_files_to_process.py
@@ -4,43 +4,45 @@ import shutil
 from smb.base import SharedFile
 from smb.SMBConnection import SMBConnection
 
-listSharedFileObj = []
+# listSharedFileObj = []
 
 # Connect to the Windows share network directory
-server_name = "192.168.10.236"
-user_name = "galtamirano@central"
-password = "Juanita7$"
-host = "induccion-colaboradores"
-remote_host = "file00.grupo-gestion.com.ar"
-domain = "central"
-use_ntlm_v2 = True
+# server_name = "192.168.10.236"
+# user_name = "galtamirano@central"
+# password = "Juanita7$"
+# host = "induccion-colaboradores"
+# remote_host = "file00.grupo-gestion.com.ar"
+# domain = "central"
+# use_ntlm_v2 = True
 
 # client = SMBConnection(user_name, password, my_name=host, remote_name=remote_host, use_ntlm_v2=use_ntlm_v2)
 # client.connect(ip=server_name, port=139, sock_family=None, timeout=60)
 
 # Define share network dir for ZIP process
-NET_WIN_DIR = "GrupoDoc\\Documentos Compartidos\\BOTSRECIBOSGG"
+# NET_WIN_DIR = "GrupoDoc\\Documentos Compartidos\\BOTSRECIBOSGG"
 # Define share network dir for processed files
-NET_WIN_DIR_FINAL = 'GrupoDoc\\Documentos Compartidos\\BOTSRECIBOSGG\\listos'
+# NET_WIN_DIR_FINAL = 'GrupoDoc\\Documentos Compartidos\\BOTSRECIBOSGG\\listos'
 
-with SMBConnection(user_name, password, my_name=host, remote_name=remote_host, domain=domain) as smbconn:
+# with SMBConnection(user_name, password, my_name=host, remote_name=remote_host, domain=domain) as smbconn:
     # "You must call this method before attempting any of the file operations with the remote server."
     # https://pysmb.readthedocs.io/en/latest/api/smb_SMBConnection.html#smb.SMBConnection.SMBConnection.connect
-    smbconn.connect(remote_host)
+    # smbconn.connect(remote_host)
 
     # Returns: A list of "smb.base.SharedFile" instances
     # https://pysmb.readthedocs.io/en/latest/api/smb_SMBConnection.html#smb.SMBConnection.SMBConnection.listPath
-    listSharedFileObj = smbconn.listPath("GrupoDoc", "/Documentos Compartidos/BOTSRECIBOSGG")
+    # listSharedFileObj = smbconn.listPath("GrupoDoc", "/Documentos Compartidos/BOTSRECIBOSGG")
 
-
-
+src = '\\\\192.168.10.236\\GrupoDoc\\Documentos Compartidos\\BOTSRECIBOSGG\\recibos.zip'
+dst = './resultados/recibos.zip'
 
 def get_files():
-    # Pre annotate for type-hinting SharedFile object
-    item: SharedFile
 
-    for item in listSharedFileObj:
-        print(item.filename)
+    shutil.copy(src, dst)
+    # Pre annotate for type-hinting SharedFile object
+    # item: SharedFile
+    #
+    # for item in listSharedFileObj:
+    #     print(item.filename)
 
     # current_path = os.getcwd()
 
