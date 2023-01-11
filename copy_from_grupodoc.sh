105202585867505657d8eac8d5d06f45fa1142bc
Gustavo Altamirano <gustavoarielaltamirano@gmail.com>
2023-01-11 00:11:11 -0300

    Proceso de identificacion de archivo zip con recibos en docker

--- copy_from_grupodoc.sh
+++ copy_from_grupodoc.sh
@@ -3,4 +3,4 @@
 src_dir="/media/grupodoc/Documentos*Compartidos/BOTSRECIBOSGG"
 dst_dir="/var/opt/apps/procesa-recibos-docker/resultados"
 
-cp -r '$src_dir'/*.zip '$dst_dir'
+cp -r "$src_dir/*.zip" "$dst_dir"
